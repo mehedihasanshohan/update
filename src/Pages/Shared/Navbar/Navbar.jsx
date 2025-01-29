fbf89ca6351561c268a5b29c2a481509d830676d
mehedi hasan <mhshohan04@gmail.com>
2025-01-29 23:50:17 +0600

    banner complete

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 
 const Navbar = () => {
   return ( <>
-    <div className="navbar fixed z-10 max-w-screen-xl opacity-50 bg-black text-white">
+    <div className="navbar fixed z-10 max-w-screen-xl opacity-50 bg-black text-white font-bold font-2xl">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -20,33 +20,23 @@ const Navbar = () => {
       </div>
       <ul
         tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        className="menu menu-sm dropdown-content bg-black rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a>HOME</a></li>
+        <li><a>CONTACT US</a></li>
+        <li><a>DASHBOARD</a></li>
+        <li><a>OUR MENU</a></li>
+        <li>OUR SHOP</li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">Bistro Boss</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+    <li><a>HOME</a></li>
+        <li><a>CONTACT US</a></li>
+        <li><a>DASHBOARD</a></li>
+        <li><a>OUR MENU</a></li>
+        <li><a href="">OUR SHOP</a></li>
     </ul>
   </div>
   <div className="navbar-end">
