dadb6f62a2f4ad4b0514eacb13b4b8eaba69c9dc
mehedi hasan <mhshohan04@gmail.com>
2025-01-29 23:39:53 +0600

    carousel done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -5,9 +5,9 @@ import Navbar from "../Pages/Shared/Navbar/Navbar"
 const Main = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
-      <Navbar></Navbar>
    </div>
 
   )
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 
 const Navbar = () => {
-  return (
-    <div className="navbar bg-base-100">
+  return ( <>
+    <div className="navbar fixed z-10 max-w-screen-xl opacity-50 bg-black text-white">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -50,9 +50,10 @@ const Navbar = () => {
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    {/* <a className="btn">Button</a> */}
   </div>
 </div>
+</>
   )
 }
 
