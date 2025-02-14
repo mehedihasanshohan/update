755975743cb7471a08be5982546129308d4f93fc
mehedi hasan <mhshohan04@gmail.com>
2025-02-14 11:19:47 +0600

    sign up and signIn pages added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,87 +1,147 @@
-// import { useState } from "react";
-// // import ReCAPTCHA from "react-google-recaptcha";
-// import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha, LoadCanvasTemplateNoReload } from "react-simple-captcha";
-
+// import { useEffect, useState} from 'react';
+// import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+// import { AuthContext } from '../../providers/AuthProviders';
+// // import { Link, useLocation, useNavigate } from 'react-router-dom';
+// import { Helmet } from 'react-helmet-async';
+// import { Link } from 'react-router-dom';
+// import { useContext } from 'react';
+// // import Swal from 'sweetalert2'
 
 // const Login = () => {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   // const [recaptchaValue, setRecaptchaValue] = useState(null);
+//     const [disabled, setDisabled] = useState(true);
+//     // eslint-disable-next-line no-undef
+//     // const { signIn } = useContext(AuthContext);
+//     const {signIn} = useContext(AuthContext);
+//     // const navigate = useNavigate();
+//     // const location = useLocation();
 
-//   // const handleRecaptchaChange = (value) => {
-//   //   setRecaptchaValue(value);
-//   // };
+//     // const from = location.state?.from?.pathname  || "/";
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     if (!recaptchaValue) {
-//       alert("Please verify the reCAPTCHA");
-//       return;
+//     useEffect(() => {
+//         loadCaptchaEnginge(6);
+//     }, [])
+
+//     const handleLogin = event => {
+//         event.preventDefault();
+//         const form = event.target;
+//         const email = form.email.value;
+//         const password = form.password.value;
+//         console.log(email, password);
+//         signIn(email, password)
+//             .then(result => {
+//                 const user = result.user;
+//                 console.log(user);
+//                 // Swal.fire({
+//                 //     title: 'User Login Successful.',
+//                 //     showClass: {
+//                 //         popup: 'animate__animated animate__fadeInDown'
+//                 //     },
+//                 //     hideClass: {
+//                 //         popup: 'animate__animated animate__fadeOutUp'
+//                 //     }
+//                 // });
+//                 // navigate(from, { replace: true });
+//             })
 //     }
-//     console.log("Email:", email);
-//     console.log("Password:", password);
-//     console.log("reCAPTCHA Token:", recaptchaValue);
-//   };
 
-//   return (
-//     <div className="max-w-md mx-auto mt-10 p-6 bg-white shadow-md rounded-lg">
-//       <h2 className="text-2xl font-bold mb-4">Login</h2>
-//       <form onSubmit={handleSubmit} className="mt-12">
-//         <div className="mb-4">
-//           <label className="block text-sm font-medium">Email</label>
-//           <input
-//             type="email"
-//             className="w-full p-2 border rounded"
-//             value={email}
-//             onChange={(e) => setEmail(e.target.value)}
-//             required
-//           />
-//         </div>
-//         <div className="mb-4">
-//           <label className="block text-sm font-medium">Password</label>
-//           <input
-//             type="password"
-//             className="w-full p-2 border rounded"
-//             value={password}
-//             onChange={(e) => setPassword(e.target.value)}
-//             required
-//           />
-//         </div>
-//         <div className="mb-4">
-//           <LoadCanvasTemplate></LoadCanvasTemplate>
-//           {/* <ReCAPTCHA
-//             sitekey="YOUR_RECAPTCHA_SITE_KEY"
-//             onChange={handleRecaptchaChange}
-//           /> */}
-//         </div>
-//         <button
-//           type="submit"
-//           className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
-//         >
-//           Sign In
-//         </button>
-//       </form>
-//     </div>
-//   );
+//     const handleValidCaptcha = (e) => {
+//         const user_captcha_value = e.target.value;
+//         if (validateCaptcha(user_captcha_value)) {
+//             setDisabled(false);
+//         }
+//         else {
+//             setDisabled(true)
+//         }
+//     }
+
+//     return (
+//         <>
+//             <Helmet>
+//                 <title>Bistro Boss | Login</title>
+//             </Helmet>
+//             <div className="hero min-h-screen bg-base-200">
+//                 <div className="hero-content flex-col md:flex-row-reverse">
+//                     <div className="text-center md:w-1/2 lg:text-left">
+//                         <h1 className="text-5xl font-bold">Login now!</h1>
+//                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+//                     </div>
+//                     <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
+//                         <form onSubmit={handleLogin} className="card-body">
+//                             <div className="form-control">
+//                                 <label className="label">
+//                                     <span className="label-text">Email</span>
+//                                 </label>
+//                                 <input type="email" name="email" placeholder="email" className="input input-bordered" />
+//                             </div>
+//                             <div className="form-control">
+//                                 <label className="label">
+//                                     <span className="label-text">Password</span>
+//                                 </label>
+//                                 <input type="password" name="password" placeholder="password" className="input input-bordered" />
+//                                 <label className="label">
+//                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+//                                 </label>
+//                             </div>
+//                             <div className="form-control">
+//                                 <label className="label">
+//                                     <LoadCanvasTemplate />
+//                                 </label>
+//                                 {/* <input onBlur={handleValidateCaptcha} type="text" name="captcha" placeholder="type the captcha above" className="input input-bordered" /> */}
+//                                 <input type="text" name='captcha' placeholder='type the captcha' className='input input-bordered' />
+//                                 <button onClick={handleValidCaptcha} className='btn btn-outline'>valid captcha</button>
+//                             </div>
+//                             <div className="form-control mt-6">
+//                                 <input disabled={disabled}  className="btn btn-primary" type="submit" value="Login" />
+//                             </div>
+//                         </form>
+//                         <p><small>New Here? <Link to="/signup">Create an account</Link> </small></p>
+//                     </div>
+//                 </div>
+//             </div>
+//         </>
+//     );
 // };
 
 // export default Login;
 
-import { useEffect} from 'react';
-import { loadCaptchaEnginge, LoadCanvasTemplate } from 'react-simple-captcha';
-// import { AuthContext } from '../../providers/AuthProvider';
-// import { Link, useLocation, useNavigate } from 'react-router-dom';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { useContext, useEffect, useState } from 'react';
+import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+import { AuthContext } from '../../providers/AuthProviders';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
-import { Link } from 'react-router-dom';
-// import Swal from 'sweetalert2'
+import Swal from 'sweetalert2'
 
 const Login = () => {
-    // const [disabled, setDisabled] = useState(true);
-    // const { signIn } = useContext(AuthContext);
-    // const navigate = useNavigate();
-    // const location = useLocation();
+    const [disabled, setDisabled] = useState(true);
+    const { signIn } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
 
-    // const from = location.state?.from?.pathname  || "/";
+    const from = location.state?.from?.pathname || "/";
 
     useEffect(() => {
         loadCaptchaEnginge(6);
@@ -93,32 +153,32 @@ const Login = () => {
         const email = form.email.value;
         const password = form.password.value;
         console.log(email, password);
-        // signIn(email, password)
-        //     .then(result => {
-        //         const user = result.user;
-        //         console.log(user);
-        //         Swal.fire({
-        //             title: 'User Login Successful.',
-        //             showClass: {
-        //                 popup: 'animate__animated animate__fadeInDown'
-        //             },
-        //             hideClass: {
-        //                 popup: 'animate__animated animate__fadeOutUp'
-        //             }
-        //         });
-        //         navigate(from, { replace: true });
-        //     })
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                Swal.fire({
+                    title: 'User Login Successful.',
+                    showClass: {
+                        popup: 'animate__animated animate__fadeInDown'
+                    },
+                    hideClass: {
+                        popup: 'animate__animated animate__fadeOutUp'
+                    }
+                });
+                navigate(from, { replace: true });
+            })
     }
 
-    // const handleValidateCaptcha = (e) => {
-    //     const user_captcha_value = e.target.value;
-    //     if (validateCaptcha(user_captcha_value)) {
-    //         setDisabled(false);
-    //     }
-    //     else {
-    //         setDisabled(true)
-    //     }
-    // }
+    const handleValidateCaptcha = (e) => {
+        const user_captcha_value = e.target.value;
+        if (validateCaptcha(user_captcha_value)) {
+            setDisabled(false);
+        }
+        else {
+            setDisabled(true)
+        }
+    }
 
     return (
         <>
@@ -152,14 +212,14 @@ const Login = () => {
                                 <label className="label">
                                     <LoadCanvasTemplate />
                                 </label>
-                                {/* <input onBlur={handleValidateCaptcha} type="text" name="captcha" placeholder="type the captcha above" className="input input-bordered" /> */}
-                                <input type="text" name='captcha' placeholder='type the captcha' className='input input-bordered' />
+                                <input onBlur={handleValidateCaptcha} type="text" name="captcha" placeholder="type the captcha above" className="input input-bordered" />
+
                             </div>
                             <div className="form-control mt-6">
-                                <input  className="btn btn-primary" type="submit" value="Login" />
+                                <input disabled={disabled} className="btn btn-primary" type="submit" value="Login" />
                             </div>
                         </form>
-                        <p><small>New Here? <Link to="/signup">Create an account</Link> </small></p>
+                        <p><small className='text-rose-500 ml-2 flex justify-center items-center mb-6'>New Here? <Link to="/signup"><i className='ml-2'> Create an account</i></Link> </small></p>
                     </div>
                 </div>
             </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable no-undef */
+
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
@@ -7,13 +7,16 @@ import {
 } from "react-router-dom";
 import "./index.css";
 import { router } from "./routes/Routes";
+import AuthProviders from "./providers/AuthProviders";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <AuthProviders>
     <HelmetProvider>
       <div className="max-w-screen-xl mx-auto">
       <RouterProvider router={router} />
       </div>
     </HelmetProvider>
+    </AuthProviders>
   </React.StrictMode>
 );
